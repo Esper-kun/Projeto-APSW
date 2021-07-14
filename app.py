61a034a4feece3cdb2a93bd2e2a4a081f75480c9
wyvian <wgcv1@aluno.ifal.edu.br>
2021-07-14 00:19:47 -0300

    alterando pagina incial

--- app.py
+++ app.py
@@ -8,13 +8,13 @@ app = Flask(__name__, template_folder=TEMPLATES, static_folder=STATIC)
 
 @app.route('/')
 def helloWorld():
-    return 'Hello World!'
+    return render_template('welcome.html')
 
 @app.route('/home')
 def home():
     data = datetime.datetime.now()
     usuarios = ['Flávio', 'Isabella', 'João', 'Maria']
-    mostrarUsuarios = True
+    mostrarUsuarios = False
     return render_template('home.html', dataAtual=data, usuarios=usuarios, mostrarUsuarios=mostrarUsuarios)
 
-#app.run(host='0.0.0.0', port=5000)
+app.run(host='0.0.0.0', port=5000)
